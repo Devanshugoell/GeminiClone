72821c5b150bbe32c451122c7e1f9bba0e8b994b
Devanshu <devgoyal952@gmail.com>
2024-07-21 17:01:20 +0530

    added functionality

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,39 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
+  const [extended, setExtended] = useState(false);
+
   return (
     <div className="sidebar min-h-screen inline-flex flex-col justify-between bg-[#f0f4f9] py-6 px-4">
       <div className="top">
         <img
+          onClick={() => setExtended(!extended)}
           className="menu block ml-[10px] cursor-pointer w-5"
           src={assets.menu_icon}
           alt="Menu Icon"
         />
-        <div className="new-chat mt-[10px] inline-flex items-center gap-[10px] py-[10px] px-4 bg-[#e6eaf1] rounded-full text-sm cursor-pointer text-gray-600 ">
+        <div className="new-chat mt-[50px] inline-flex items-center gap-[10px] py-[10px] px-4 bg-[#e6eaf1] rounded-full text-sm cursor-pointer text-gray-600 ">
           <img className="w-5" src={assets.plus_icon} alt="add icon" />
-          <p className="text-slate-700">New Chat</p>
+          {extended ? <p className="text-slate-700">New Chat</p> : null}
         </div>
-        <div className="recent ">
-          <p className="recent-title flex flex-col">Recent</p>
-          <div className="recent-entry">
-            <img className="w-5" src={assets.message_icon} alt="message icon" />
-            <p>What is react....</p>
+        {extended ? (
+          <div className="recent flex flex-col">
+            <p className="recent-title mt-8 mb-5 ">Recent</p>
+            <div className="recent-entry flex items-start gap-[10px] p-[10px] pr-10 rounded-full cursor-pointer text-[#282828] hover:bg-[#e2e6eb]">
+              <img
+                className="w-5"
+                src={assets.message_icon}
+                alt="message icon"
+              />
+              <p>What is react....</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
-      <div className="bottom">
-        <div className="bottom-item recent-entry">
+      <div className="bottom flex flex-col">
+        <div className="bottom-item recent-entry flex items-start gap-[10px] p-[10px] pr-10 rounded-full cursor-pointer text-[#282828] hover:bg-[#e2e6eb]">
           <img className="w-5" src={assets.question_icon} alt="question icon" />
-          <p>Help</p>
+          {extended ? <p>Help</p> : null}
         </div>
-        <div className="bottom-item recent-entry">
+        <div className="bottom-item recent-entry flex items-start gap-[10px] p-[10px] pr-10 rounded-full cursor-pointer text-[#282828] hover:bg-[#e2e6eb]">
           <img className="w-5" src={assets.history_icon} alt="question icon" />
-          <p>Activity</p>
+          {extended ? <p>Activity</p> : null}
         </div>
-        <div className="bottom-item recent-entry">
+        <div className="bottom-item recent-entry flex items-start gap-[10px] p-[10px] pr-10 rounded-full cursor-pointer text-[#282828] hover:bg-[#e2e6eb]">
           <img className="w-5" src={assets.setting_icon} alt="question icon" />
-          <p>Setting</p>
+          {extended ? <p>Setting</p> : null}
         </div>
       </div>
     </div>
