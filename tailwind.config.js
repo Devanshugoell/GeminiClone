00178d7fc015e2e7392d4306e44b87873d216516
Devanshu <devgoyal952@gmail.com>
2024-07-22 12:00:35 +0530

    added utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,25 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        // This is called utility classes
+        //By using these utility classes, you can apply the custom styles defined in your Tailwind CSS configuration.
+
+        ".bg-gradient-text": {
+          background: "-webkit-linear-gradient(16deg, #4b90ff, #ff5546)",
+          "-webkit-background-clip": "text",
+          "-webkit-text-fill-color": "transparent",
+        },
+        ".grid-temp-col": {
+          "grid-template-columns": "repeat(autofill,minmax(180px,1fr))",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+    // After defining the custom utility, you can use it in your HTML or JSX code like any other Tailwind class.
+    // This is called adding custom utility
+  ],
 };
