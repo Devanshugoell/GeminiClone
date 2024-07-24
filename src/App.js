a897f82fe047dbb7ebd4530273c05d5b4db4f30b
Devanshu <devgoyal952@gmail.com>
2024-07-24 12:04:36 +0530

    installed google/generative ai

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Main from "./components/Main/main";
+import ContextProvider from "./context/context";
 
 function App() {
   return (
-    <>
+    <ContextProvider>
       <Sidebar />
       <Main />
-    </>
+    </ContextProvider>
   );
 }
 
