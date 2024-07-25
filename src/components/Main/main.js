4e796ad31ef5aacd5dadb86d463641e8508ca72b
Devanshu <devgoyal952@gmail.com>
2024-07-25 12:21:55 +0530

    changed height of animation

--- src/components/Main/main.js
+++ src/components/Main/main.js
@@ -89,9 +89,9 @@ const Main = () => {
               <img className="w-10" src={assets.gemini_icon} alt="gemini" />
               {loading ? (
                 <div className="loader w-full flex flex-col gap-[10px]">
-                  <hr className="rounded-md border-none bg-[#f6f7f8] bg-gradient-to-r from-slate-100 to-indigo-300 bg-[length:800px_50px] h-5 animate-loader1" />
-                  <hr className="rounded-md border-none bg-[#f6f7f8] bg-gradient-to-r from-slate-100 to-indigo-300 bg-[length:800px_50px] h-5 animate-loader2" />
-                  <hr className="rounded-md w-2/4 border-none bg-[#f6f7f8] bg-gradient-to-r from-slate-100 to-indigo-300 bg-[length:800px_50px] h-5 animate-loader3 " />
+                  <hr className="rounded-md border-none bg-[#f6f7f8] bg-gradient-to-r from-slate-100 to-indigo-300 bg-[length:800px_50px] h-4 animate-loader1" />
+                  <hr className="rounded-md border-none bg-[#f6f7f8] bg-gradient-to-r from-slate-100 to-indigo-300 bg-[length:800px_50px] h-4 animate-loader2" />
+                  <hr className="rounded-md w-2/4 border-none bg-[#f6f7f8] bg-gradient-to-r from-slate-100 to-indigo-300 bg-[length:800px_50px] h-4 animate-loader3 " />
                 </div>
               ) : (
                 <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
